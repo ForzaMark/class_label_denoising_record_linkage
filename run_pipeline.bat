6ede2b36c98dcc8417698200447a91156e02ee8b
Mark Heimer <mark.heimer@t2informatik.de>
2025-09-30 13:48:30 +0200

    minor pipeline adjustments

--- run_pipeline.bat
+++ run_pipeline.bat
@@ -1,8 +1,7 @@
 @echo off
 
-set TF_ENABLE_ONEDNN_OPTS=0
 set dataset=music
-set RPATH="C:\Program Files\R\R-4.4.3\bin\Rscript.exe"
+set RPATH="C:\Program Files\R\R-4.3.1\bin\Rscript.exe"
 
 %RPATH% %SCRIPT% %DATASET%
 
@@ -21,12 +20,12 @@ python 2_introduce_artificial_noise.py %dataset%
 
 echo ################### DENOISING ###################
 python 3_denoising_evaluation.py %dataset%
-%RPATH% ".\src\r_programming\3_denoise_evaluation_scenarios.R" %dataset%
+%RPATH% ".\r_programming\3_denoise_evaluation_scenarios.R" %dataset%
 
 echo ################### DOWNSTREAM CLASSIFICATION ###################
 python 4_downstream_performance_evaluation.py train_test_split %dataset%
 python 4_downstream_performance_evaluation.py cleaning %dataset%
-%RPATH% ".\src\r_programming\3_denoise_evaluation_scenarios_downstream_classifier.R" %dataset%
+%RPATH% ".\r_programming\3_denoise_evaluation_scenarios_downstream_classifier.R" %dataset%
 python 4_downstream_performance_evaluation.py evaluation %dataset%
 
 pause
